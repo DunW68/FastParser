286b5c8cd0b5c109fd5fb4f6e636f582b2b76268
DunW68 <zolberg68@mail.ru>
2023-03-24 13:17:42 +0300

    Changed post methods for saving articles in db

--- FastParser/db/parsers/article_parser/requests.py
+++ FastParser/db/parsers/article_parser/requests.py
@@ -9,7 +9,7 @@ class ArticleRequests:
         self.db_session = db_session
 
     def create_record(self, article: schemas.ArticleParserBase):
-        article = models.Article(header=article.header, text=article.text)
+        article = models.Article(page_url=article.page_url, header=article.header, text=article.text)
         self.db_session.add(article)
         self.db_session.commit()
         self.db_session.refresh(article)
@@ -22,9 +22,9 @@ class ArticleImagesRequests:
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
-    def create_record(self, article_image: schemas.ArticleImage, article_id):
-        article_image = models.ImageUrls(page_url=article_image.page_url, image_url=article_image.image_url,
-                                         article_id=article_id)
+    def create_record(self, article_image: schemas.ArticleImage):
+        article_image = models.ImageUrls(image_url=article_image.image_url,
+                                         article_id=article_image.article_id)
         self.db_session.add(article_image)
         self.db_session.commit()
         self.db_session.refresh(article_image)
--- FastParser/parsers/routing.py
+++ FastParser/parsers/routing.py
@@ -1,3 +1,4 @@
+import sqlalchemy.exc
 from fastapi import Depends, FastAPI, Header, HTTPException, APIRouter
 from starlette.status import HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND, HTTP_201_CREATED
 from FastParser.db.configs import Base, engine, ArticleParserSession
@@ -26,19 +27,25 @@ class ArticleParser:
         article_text = url_parser.get_article_text()
         images = url_parser.get_article_images()
         response = GetArticle(header=header,
-                             text=article_text,
-                             images=images
+                              text=article_text,
+                              images=images
                              )
         return response
 
     @router.post("/", status_code=HTTP_201_CREATED)
-    def post_article(self, url: AnyUrl) -> str:
+    def post_article(self, url: AnyUrl) -> dict:
         url_parser = ParseUrl(url=url)
         header = url_parser.get_header()
         article_text = url_parser.get_article_text()
-        article = ArticleParserBase(header=header, text=article_text)
-        self.article_requests.create_record(article=article)
-        return "Successfully parsed"
+        article = ArticleParserBase(page_url=url, header=header, text=article_text)
+        try:
+            a = self.article_requests.create_record(article=article)
+            response = {"detail": "Successfully parsed"}
+        except sqlalchemy.exc.IntegrityError:
+            response = {"detail": "Already exists"}
+        return response
+
+
 
 
 parser.include_router(router, prefix="/article_parser")
